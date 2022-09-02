a0e835f0525f44f5439072bdb84d37da9e6a16da
Bruno Rocha de Lima <brunorocha33@live.com>
2022-09-02 11:40:44 -0300

    iniciando autenticação

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Series;
 use App\Http\Controllers\Api\SeriesController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 /* |-------------------------------------------------------------------------- | API Routes |-------------------------------------------------------------------------- | | Here is where you can register API routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | is assigned the "api" middleware group. Enjoy building your API! | */
 
@@ -12,9 +15,44 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // rota manual
-Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series', [SeriesController::class , 'index']);
 
 // rotas automáticas
 Route::apiResources([
     '/series' => SeriesController::class]);
 
+//Login
+// Route::post('/login', function (Request $request) {
+//     $credentials = $request->only(['email', 'password']);
+
+//     if (Auth::attempt($credentials) === false) {
+//         return response()->json('Unauthorized', 401);
+//     }
+
+//     $user = Auth::user();
+//      dd($user);
+
+// });
+
+//Login
+Route::post('/login', function (Request $request) {
+
+    $credentials = $request->only(['email', 'password']);
+    
+    $user = User::where('email', $credentials['email'])->first();
+    $password = User::where('password', $credentials['password'])->first();
+
+    // if(Auth::attempt($credentials) === false) {
+    //     return response()->json('Unauthorized', 401);
+    // }
+    
+    if(!$user || !$password) {
+        return response()->json('Unauthorized', 401);
+    }
+        
+    dd($user);
+
+
+ 
+
+});
